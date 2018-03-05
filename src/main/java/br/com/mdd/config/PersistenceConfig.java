763a00f6aacbf839664856fcf4429a12d2889af4
Rafael Alves Farias <rafael.afarias@b2wdigital.com>
2018-03-05 19:12:56 -0300

    configuracoes de infra
    configuracao de logs e de arquivos de configuracao

--- src/main/java/br/com/mdd/config/PersistenceConfig.java
+++ src/main/java/br/com/mdd/config/PersistenceConfig.java
@@ -25,7 +25,7 @@ import br.com.mdd.domain.model.VariableExpense;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource({ "classpath:persistence-mysql.properties" })
+@PropertySource({ "file:${app_root_dir}/conf/persistence-mysql.properties" })
 public class PersistenceConfig {
 	
 	@Autowired
